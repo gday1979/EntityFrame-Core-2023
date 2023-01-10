5d6c41fef15c5e55acf3e37b27b9865313340afd
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-10 18:19:58 +0200

    Add project files.

---
+++ EntityFrame Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrame_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
