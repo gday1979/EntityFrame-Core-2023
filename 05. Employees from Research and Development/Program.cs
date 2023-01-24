5c527b49a4b995b725bc71609c16121e7c5eddce
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-24 18:28:06 +0200

    commit

---
+++ 05. Employees from Research and Development/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Employees_from_Research_and_Development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
