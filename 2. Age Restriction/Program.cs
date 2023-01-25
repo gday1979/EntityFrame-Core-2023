191894150ccf5f40226a2b7157ed7619be338ca4
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-25 20:08:06 +0200

    update

---
+++ 2. Age Restriction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Age_Restriction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 4. Books by Price/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._Books_by_Price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
