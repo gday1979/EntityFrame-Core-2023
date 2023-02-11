85fcb07ccc05ce2a249015a5e1ac12f2684ff22e
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-11 19:27:55 +0200

    commit

---
+++ 06. Adding a New Address and Updating Employee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Adding_a_New_Address_and_Updating_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 07. Employees and Projects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._Employees_and_Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
