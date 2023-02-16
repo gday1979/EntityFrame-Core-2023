1499184d081a5877659b0cc31bc6def8598bf9b1
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-16 20:13:20 +0200

    commit

--- SoftUni/Program.cs
+++ SoftUni/Program.cs
@@ -1,10 +1,8 @@
-﻿namespace SoftUni
+﻿namespace SoftUni;
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
