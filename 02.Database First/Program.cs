5369f2001e13022737b1ff642c909a95fdf1a4bf
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-03-03 11:42:02 +0200

    update

---
+++ 02.Database First/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02.Database_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
